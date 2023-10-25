979376dcadd9d0e177723867525803a91e7156f8
1985Valdemar <vteider@yahoo.com>
2023-10-25 14:03:49 -0300

    Atualiando Roteamento

--- Roteamento/roteamento/src/app/app-routing/app-routing.module.ts
+++ Roteamento/roteamento/src/app/app-routing/app-routing.module.ts
@@ -4,18 +4,29 @@ import { PrimeiraPaginaComponent } from '../primeira-pagina/primeira-pagina.comp
 import { SegundaPaginaComponent } from '../segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from '../pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaComParametrosComponent } from '../pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from '../pagina-protegida/pagina-protegida.component';
+import { authGuard } from '../auth.guard';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }from '@angular/forms';
+import { AppComponent } from '../app.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes =[
   {path: "primeira-pagina", component: PrimeiraPaginaComponent},
   {path:"segunda-pagina", component: SegundaPaginaComponent},
   {path:"", redirectTo:"primeira-pagina", pathMatch:'full'},
   {path:"pagina-com-parametros/:id",component: PaginaComParametrosComponent},
+  {path:"login",component:LoginComponent},
   {path:"**", component: PaginaNaoEncontradaComponent}
 ]
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes),
+             FormsModule,BrowserModule],
+  exports: [RouterModule],
+  bootstrap:[AppComponent]
 })
 export class AppRoutingModule { }
+
+export class AppModule { }
---
+++ Roteamento/roteamento/src/app/auth.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+
+Injectable({
+  providedIn: 'root'
+})
+
+export const authGuard: CanActivateFn = (route, state) => {
+  
+  route: ActivatedRouteSnapshot
+  state: RouterStateSnapshot 
+  return false;
+};
